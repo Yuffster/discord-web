23c1bbbadda8a9dfa838b309020fe98ed7e5379c
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-01-16 20:43:51 -0800

    Added configuration options (with defaults) for view directory and view engine.

--- server.js
+++ server.js
@@ -9,18 +9,19 @@ exports.start = function(config) {
 
 	app.use(express.static(config.world_path + '/public'));
 	app.use(express.static(__dirname + '/public'));
-	
-	app.set('view engine', 'ejs');
+
+	app.set('views', (config.views || __dirname+'/views'));
+	app.set('view engine', (config.view_engine || 'ejs' ));
 
 	moo.listen(app, '/scripts/mootools.js')
 
 	app.get('/', function(res, req) {
-		req.render('index', {game_title:config.world_name});
+		req.render('index', {world_name:config.world_name});
 	});
 
 	var io = require('socket.io').listen(app);
 	io.sockets.on('connection', function (socket) {
-		var conn = net.createConnection(config.game_port);
+		var conn = net.createConnection(config.world_port);
 		conn.setEncoding('utf-8');
 		conn.on("data", function(data) {
 			socket.emit('data', data);
